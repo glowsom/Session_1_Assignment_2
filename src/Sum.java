67e059fd55eafb11857b82433af1209a040a1894
Samuel O. Appiah <me@samobeng.com>
2017-06-07 00:13:37 -0500

    Session 1 Assignment 2

---
+++ src/Sum.java
@@ -0,0 +1,16 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Sum of 2 numbers
+		int a = 2;
+		int b = 3;
+		int c = a;
+		
+		c += b;
+		
+		System.out.println( a + " + " + b + " = " + c );
+
+	}
+
+}
